59a4c960fbef7a8e56500ca5180778da4119226b
SyvonenkoOleh <115041334+SyvonenkoOleh@users.noreply.github.com>
2023-04-04 11:59:06 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,113 +1,126 @@
-import NewsApiService from './js/api-service';
-import { lightbox } from './js/lightbox';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+'use strict';
 
-const refs = {
-  searchForm: document.querySelector('.search-form'),
-  galleryContainer: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
-};
-let isShown = 0;
-const newsApiService = new NewsApiService();
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import axios from 'axios';
 
-refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+const searchQuery = document.querySelector('input[name="searchQuery"]');
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const clear = elems => [...elems.children].forEach(div => div.remove());
+const loadBtn = document.querySelector('.load-more');
+const lightbox = () => new SimpleLightbox('.gallery a', {});
+let perPage = 40;
+let page = 0;
+let name = searchQuery.value;
 
-const options = {
-  rootMargin: '50px',
-  root: null,
-  threshold: 0.3,
-};
-const observer = new IntersectionObserver(onLoadMore, options);
+loadBtn.style.display = 'none';
 
-function onSearch(e) {
-  e.preventDefault();
-
-  refs.galleryContainer.innerHTML = '';
-  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-  newsApiService.resetPage();
-
-  if (newsApiService.query === '') {
-    Notify.warning('Please, fill the main field');
-    return;
-  }
-
-  isShown = 0;
-  fetchGallery();
-  onRenderGallery(hits);
-}
-
-function onLoadMore() {
-  newsApiService.incrementPage();
-  fetchGallery();
-}
-
-async function fetchGallery() {
-  refs.loadMoreBtn.classList.add('is-hidden');
-
-  const r = await newsApiService.fetchGallery();
-  const { hits, total } = r;
-  isShown += hits.length;
-
-  if (!hits.length) {
-    Notify.failure(
-      `Sorry, there are no images matching your search query. Please try again.`
+async function fetchImages(name, page) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=23580980-4f75151f85975025bb6074227&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
     );
-    refs.loadMoreBtn.classList.add('is-hidden');
-    return;
-  }
-
-  onRenderGallery(hits);
-  isShown += hits.length;
-
-  if (isShown < total) {
-    Notify.success(`Hooray! We found ${total} images !!!`);
-    refs.loadMoreBtn.classList.remove('is-hidden');
-  }
-
-  if (isShown >= total) {
-    Notify.info("We're sorry, but you've reached the end of search results.");
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
 }
 
-function onRenderGallery(elements) {
-  const markup = elements
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-    <a href="${largeImageURL}">
-      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        ${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        ${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        ${downloads}
-      </p>
-    </div>
-    </div>`;
+async function eventHandler(ev) {
+  ev.preventDefault();
+  clear(gallery);
+  loadBtn.style.display = 'none';
+  page = 1;
+  name = searchQuery.value;
+  console.log(name);
+  fetchImages(name, page)
+    .then(name => {
+      console.log(`Number of arrays: ${name.hits.length}`);
+      console.log(`Total hits: ${name.totalHits}`);
+      let totalPages = Math.ceil(name.totalHits / perPage);
+      console.log(`Total pages: ${totalPages}`);
+
+      if (name.hits.length > 0) {
+        Notiflix.Notify.success(`Hooray! We found ${name.totalHits} images.`);
+        renderGallery(name);
+        console.log(`Current page: ${page}`);
+        lightbox();
+
+        if (page < totalPages) {
+          loadBtn.style.display = 'block';
+        } else {
+          loadBtn.style.display = 'none';
+          console.log('There are no more images');
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+        clear(gallery);
       }
-    )
-    .join('');
-  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
+    })
+    .catch(error => console.log(error));
 }
+
+searchForm.addEventListener('submit', eventHandler);
+
+function renderGallery(name) {
+  const markup = name.hits
+    .map(hit => {
+      return `<div class="photo-card">
+      <a href="${hit.largeImageURL}"> <img class="photo-img" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" /></a>
+      <div class="info">
+        <p class="info-item">
+          <p><b>Likes</b> <br>${hit.likes}</br></p>
+        </p>
+        <p class="info-item">
+          <p><b>Views</b> <br>${hit.views}</br></p>
+        </p>
+        <p class="info-item">
+          <p><b>Comments</b> <br>${hit.comments}</br></p>
+        </p>
+        <p class="info-item">
+          <p><b>Downloads</b> <br>${hit.downloads}</br></p>
+        </p>
+      </div>
+    </div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+loadBtn.addEventListener(
+  'click',
+  () => {
+    name = searchQuery.value;
+    console.log('load more images');
+    page += 1;
+    fetchImages(name, page).then(name => {
+      let totalPages = Math.ceil(name.totalHits / perPage);
+      renderGallery(name);
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+ 
+      lightbox().refresh();
+      console.log(`Current page: ${page}`);
+
+      if (page >= totalPages) {
+        loadBtn.style.display = 'none';
+        console.log('There are no more images');
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      }
+    });
+  },
+  true,
+);
